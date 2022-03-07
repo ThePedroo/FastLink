6f71363ed96628bc6bfc13a96ff091861a05a84f
ThePedro <73797168+ThePedroo@users.noreply.github.com>
2022-03-07 01:55:54 -0300

    Upgrade & Add User-Agent header.

--- request.js
+++ request.js
@@ -4,16 +4,32 @@ import http from 'http'
 function makeRequest(url, options) {
   return new Promise((resolve) => {
     let req, data = ''
+    
+    options.headers['User-Agent'] = 'FastLink@1.1.5'
 
     if (url.startsWith('https://')) {
-      req = https.request(url, { port: options.port, method: options.method, headers: options.header }, (res) => {
+      req = https.request(url, { port: options.port, method: options.method, headers: options.headers }, (res) => {
         res.on('data', (chunk) => data += chunk)
-        res.on('end', () => resolve(data ? JSON.parse(data) : data))
+        res.on('end', () => {
+          try {
+            let json = JSON.parse(data)
+            resolve(json)
+          } catch(err) {
+            resolve(data)
+          }
+        })
       })
     } else if (url.startsWith('http://')) {
-      req = http.request(url, { port: options.port, method: options.method, headers: options.header }, (res) => {
+      req = http.request(url, { port: options.port, method: options.method, headers: options.headers }, (res) => {
         res.on('data', (chunk) => data += chunk)
-        res.on('end', () => resolve(data ? JSON.parse(data) : data))
+        res.on('end', () => {
+          try {
+            let json = JSON.parse(data)
+            resolve(json)
+          } catch(err) {
+            resolve(data)
+          }
+        })
       })
     }
 
