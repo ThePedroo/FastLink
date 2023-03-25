bcd59f2580744bf8967969812649e223e35db27f
TehPig (TechPig) <34095153+TehPigYT@users.noreply.github.com>
2023-03-25 22:39:51 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -173,16 +173,16 @@ function onMessage(data, Infos, map, node) {
                   }
                 })
                 switch (players[data.guildId].loop) {
-                 case 'track';
-                  // Do not modify queue
-                  break;
-                 case 'queue'; 
-                  queue[data.guildId].shift()
-                  queue[data.guildId].push(queue[data.guildId][0]);
-                  break;
-                 default;
-                  queue[data.guildId].shift();
-                  break;
+                  case 'track':
+                    // Do not modify queue
+                    break;
+                  case 'queue':
+                    queue[data.guildId].shift();
+                    queue[data.guildId].push(queue[data.guildId][0]);
+                    break;
+                  default:
+                    queue[data.guildId].shift();
+                    break;
                 }
               }
               if (data.reason == 'REPLACED') queue[data.guildId].shift()
